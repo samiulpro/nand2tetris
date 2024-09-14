de5255d99a50a8f2960c18900485d959ff466686
Samiul Islam Sami <samiul16205@gmail.com>
2024-09-15 01:24:26 +0600

    working on Fill.asm

---
+++ project4/Fill.asm
@@ -0,0 +1,18 @@
+// This file is part of www.nand2tetris.org
+// and the book "The Elements of Computing Systems"
+// by Nisan and Schocken, MIT Press.
+// File name: projects/4/Fill.asm
+
+// Runs an infinite loop that listens to the keyboard input. 
+// When a key is pressed (any key), the program blackens the screen,
+// i.e. writes "black" in every pixel. When no key is pressed, 
+// the screen should be cleared.
+
+// Samiul Islam Sami
+
+(INFINITE_KEYBOARD_LOOP)
+    @24576
+    D=M
+    @INFINITE_KEYBOARD_LOOP
+    0;JMP
+    
