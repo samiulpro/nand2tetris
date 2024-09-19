ceeb68cb58d267df19f57464397fe5ee07fa98af
Samiul Islam Sami <samiul16205@gmail.com>
2024-09-20 02:20:43 +0600

    project4_done

--- project4/Fill.asm
+++ project4/Fill.asm
@@ -10,9 +10,54 @@
 
 // Samiul Islam Sami
 
-(INFINITE_KEYBOARD_LOOP)
-    @24576
-    D=M
-    @INFINITE_KEYBOARD_LOOP
+
+@SCREEN
+D=A
+@addr
+M=D
+
+
+(REFRESH)
+	@KBD
+	D=M
+	@LOOP_BLACK
+	D;JNE
+	@LOOP_WHITE
+	D;JEQ
+
+(LOOP_BLACK)
+	@addr
+	A=M
+	M=-1
+
+	@INCREMENT_ADDR
+	0;JMP
+
+(LOOP_WHITE)
+	@addr
+	A=M
+	M=0
+
+	@INCREMENT_ADDR
+	0;JMP
+
+(INCREMENT_ADDR)
+	@addr      // Load the current screen address
+    D=M        // D = addr
+    @24575     // The last screen address
+    D=D-A      // Check if addr == 24575
+    @RESET_ADDR
+    D;JEQ      // If addr == 24575, reset to SCREEN base
+
+    @addr
+    M=M+1      // Increment the addr to the next screen register
+    @REFRESH   // Return to REFRESH loop
     0;JMP
-    
+
+(RESET_ADDR)
+    @SCREEN    // Load SCREEN base address
+    D=A        // D = SCREEN
+    @addr      // Reset addr to SCREEN
+    M=D        // addr = SCREEN
+    @REFRESH   // Return to REFRESH loop
+    0;JMP
